f05a6fd3de941771d3c03eb4d61f0ca14381ccd3
Dmitry_Razbitsky <z1a@ya.ru>
2021-06-21 01:04:55 +0300

    Update child_name2.py

--- child_name2.py
+++ child_name2.py
@@ -16,7 +16,7 @@ print('Нажмите "1" чтобы узнать именя ваших буду
 b = str(input())
 
 if b == '1':
-    print('Мужское имя: ' + random.choice(z[113:168])  + '\n' + 'Женское имя: ' + random.choice(z[57:112]))
+    print('Мальчик: ' + random.choice(z[113:168])  + '\n' + 'Девочка: ' + random.choice(z[57:112]))
 else:
     print('Вы ошиблись')
 
